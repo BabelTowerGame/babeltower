721fa59f37321f5ac37200c18bfa68ca14ef8ccd
bowiegou <i@bowiegou.com>
2018-01-25 16:28:14 -0500

    project structure init

---
+++ Assets/Scripts/Editor/Cheat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Cheat : EditorWindow {
+    [MenuItem("Window/Cheat")]
+    public static void ShowWindow() {
+        EditorWindow.GetWindow(typeof(Cheat));
+    }
+
+    void OnGUI() {
+
+        GUILayout.Label("Character", EditorStyles.boldLabel);
+        GUILayout.Label("Level", EditorStyles.boldLabel);
+        GUILayout.Label("Mob", EditorStyles.boldLabel);
+
+        // Some cheat use to skip levels and debug
+
+
+    }
+
+}
