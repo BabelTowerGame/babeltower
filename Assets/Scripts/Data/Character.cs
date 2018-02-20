06d9c7bfdfc7305fcb45c4c9cf14843e614052bd
Yibo Gou <bowie@MSI.localdomain>
2018-02-19 22:24:02 -0500

    character class prototype

--- Assets/Scripts/Data/Character.cs
+++ Assets/Scripts/Data/Character.cs
@@ -1,6 +1,45 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
+[Serializable]
 public class Character {
+    #region general
+    public string name;
+    public float maxHealth;
+    public int level;
+    #endregion
+
+    #region status
+    public bool inBattle;
+    public bool inMovement;
+    public bool isDead {
+        get { return currentHealth <= 0; }
+    }
+
+    //public Inventory inventory;
+    public float currentHealth;
+    //public Location location
+    public float Defense {
+        get { return 0; }
+    }
+    public float Damage {
+        get { return 0; }
+    }
+    #endregion
+
+    public struct Equipped {
+        //public Weapon weapon;
+        //public Armor head;
+        //public Armor chest;
+        //public Armor legs;
+        //public Shoe shoe;
+    }
+   
+
+
+
 
 }
