557a1a16b005ab6f1a71de0096a2205c79b526b1
Charles Ma <1995charles@gmail.com>
2018-02-19 22:51:24 -0500

    <ADD> Data class Monster, Location, World, WorldLevel.

---
+++ Assets/Scripts/Data/Location.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Location {
+	public int level;
+	public Point point;
+
+
+}
+
+struct Point{
+	public int X { get; set;}
+	public int Y { get; set;}
+}
