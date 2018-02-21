24efdcfb3c83145f82e0dca373e46189f37e8e08
bowiegou <i@bowiegou.com>
2018-02-20 22:49:14 -0500

    added loader and game manager skeleton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager instance = null;
+
+    //private DataManager dataManager;
+
+    public GameManager Instance {
+        get { return GameManager.instance; }
+    }
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+        initGame();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //TODO: get update from network interface
+        //      sync local changes to network partners
+        //      update game data and graphics
+		
+	}
+
+    private void initGame() {
+        //TODO: init game data from const field,
+        //      load game state and init network interface
+       
+    }
+}
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+
+    public GameObject GameManager;
+    public GameObject SoundManager;
+
+    void Awake() {
+        //Check if a GameManager has already been assigned to static variable GameManager.instance or if it's still null
+        if (GameManager.Instance == null)
+
+            //Instantiate gameManager prefab
+            Instantiate(GameManager as GameObject);
+    }
+
+
+}
