3ddcecf992c9afc459d6306b1db2d3e74e9f95c9
Jiaqi Zhu <zhu442@purdue.edu>
2018-02-20 20:15:06 -0500

    add get-set function into item and subclasses

--- Assets/Scripts/Data/Item.cs
+++ Assets/Scripts/Data/Item.cs
@@ -18,6 +18,10 @@ public class Item {
         get { return this.id; }
         set { this.id = value; }
     }
+	public int QualityLevel{
+		get{ return this.level; }
+		set{ this.level = value; }
+	}
     public Item(string name, int level) {
         this.name = name;
         this.level = level;
@@ -29,6 +33,18 @@ public class Weapon : Item {
     private int damage;
     private Ability ab;
     private int cd;
+	public int Damage{
+		get{ return this.damage; }
+		set{ this.damage = value; }
+	}
+	public Ability Ab{
+		get{ return this.ab; }
+		set{ this.ab = value; }
+	}
+	public int Cd{
+		get{ return this.cd; }
+		set{ this.cd = value; }
+	}
     public Weapon(string name, int level, int damage, Ability ab, int cd) : base(name, level) {
         this.damage = damage;
         this.ab = ab;
@@ -42,6 +58,10 @@ public class Shield : Weapon {
     public Shield(string name, int level, int damage, Ability ab, int cd, int defense) : base(name, level, damage, ab, cd) {
         this.defense = defense;
     }
+	public int Defense{
+		get{ return this.defense; }
+		set{ this.defense = value; }
+	}
 }
 
 [Serializable]
@@ -53,6 +73,14 @@ public class Armor : Item {
     }
     private armor_type type;
     private int defense;
+	public armor_type Armor_Type{
+		get{ return this.type; }
+		set{ this.type = value; }
+	}
+	public int Defense{
+		get{ return this.defense; }
+		set{ this.defense = value; }
+	}
     public Armor(string name, int level, armor_type type, int defense) : base(name, level) {
         this.defense = defense;
         this.type = type;
@@ -62,6 +90,10 @@ public class Armor : Item {
 [Serializable]
 public class Shoes : Item {
     private int speed;
+	public int Speed{
+		get{ return this.speed; }
+		set{ this.speed = ValueType; }
+	}
     public Shoes(string name, int level, int speed) : base(name, level) {
         this.speed = speed;
     }
@@ -73,6 +105,22 @@ public class Ability {
     private int A_range;
     private int A_type;
     private int A_cd;
+	public int Ability_damage{
+		get{return this.A_damage; }
+		set{ this.A_damage = value; }
+	}
+	public int Ability_range{
+		get{ return this.A_range; }
+		set{ this.A_range = value; }
+	}
+	public int Ability_type{
+		get{ return this.A_type; }
+		set{ this.A_type = value; }
+	}
+	public int Ability_cd{
+		get{ return this.A_cd; }
+		set{ this.A_cd = value; }
+	}
     public Ability(int damage, int range, int type, int cd) {
         this.A_damage = damage;
         this.A_range = range;
