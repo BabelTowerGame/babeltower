92837f124cbfa50a4937c3114f0709021ccde326
bowiegou <i@bowiegou.com>
2018-02-20 21:34:57 -0500

    added ConstIntDB, fix ItemDB typo

---
+++ Assets/Scripts/Data/Database/ConstIntDB.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+using System;
+
+public class ConstIntDB : ScriptableObject {
+
+    #region singleton
+    private static ConstIntDB m_Instance;
+    public static ConstIntDB Instance {
+        get {
+            if (m_Instance == null)
+                m_Instance = Resources.Load("Databases/ConstIntDB") as ConstIntDB;
+
+            return m_Instance;
+        }
+    }
+    #endregion
+
+    public Pair<string,int>[] items;
+
+    public Pair<string, int> get(int index) {
+        return (this.items[index]);
+    }
+
+    public Pair<string, int> getByKey(string key) {
+        for (int i = 0; i < this.items.Length; i++) {
+            if (this.items[i].First.Equals(key))
+                return this.items[i];
+        }
+
+        return null;
+    }
+}
