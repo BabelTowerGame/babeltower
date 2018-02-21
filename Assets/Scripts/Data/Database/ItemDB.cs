0cd628b70a5944453c962a232ee88ef348d9a21b
bowiegou <i@bowiegou.com>
2018-02-20 19:49:38 -0500

    created ItemDB

---
+++ Assets/Scripts/Data/Database/ItemDB.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class ItemDB : ScriptableObject {
+
+    #region singleton
+    private static ItemDB m_Instance;
+    public static ItemDB Instance {
+        get {
+            if (m_Instance == null)
+                m_Instance = Resources.Load("Databases/ItemDB") as ItemDB;
+
+            return m_Instance;
+        }
+    }
+    #endregion
+
+    public Item[] items;
+
+    public Item get(int index) {
+        return (this.items[index]);
+    }
+
+    public Item getByID(int ID) {
+        for (int i = 0; i < this.items.Length; i++) {
+            if (this.items[i].ID == ID)
+                return this.items[i];
+        }
+
+        return null;
+    }
+
+    public Item getByName(string name) {
+        for (int i = 0; i < this.items.Length; i++) {
+            if (this.items[i].Name.Equals(name))
+                return this.items[i];
+        }
+
+        return null;
+    }
+}
