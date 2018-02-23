c99afc218984527acdaeb44d38065dee70d5f41b
bowiegou <i@bowiegou.com>
2018-02-22 22:16:01 -0500

    Optimize Input Field on char create scene

--- Assets/Scripts/InputFieldHandler.cs
+++ Assets/Scripts/InputFieldHandler.cs
@@ -1,16 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class InputFieldHandler : MonoBehaviour {
+[RequireComponent(typeof(InputField))]
+public class InputFieldHandler : MonoBehaviour, ISelectHandler, IDeselectHandler {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private InputField field;
+    public string placeholder;
+
+    public void Start() {
+        this.field = this.gameObject.GetComponent<InputField>();
+        this.field.text = placeholder;
+    }
+
+    public void OnDeselect(BaseEventData eventData) {
+        //Debug.Log(field.text.Trim() + " " + string.IsNullOrEmpty(field.text.Trim()));
+        if (string.IsNullOrEmpty(field.text.Trim())) {
+            field.text = placeholder;
+        }
+    }
+
+    public void OnSelect(BaseEventData eventData) {
+        if (field.text == placeholder) {
+            field.text = "";
+        }
+    }
 }
