dba3864769e948120fcc1c4988f7824a1c7a747f
bowiegou <i@bowiegou.com>
2018-03-27 22:47:38 -0400

    added character to scene; added terrain to scene

---
+++ Assets/Scripts/EquipmentSystem/BodyPart.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EasyEquipmentSystem
+{
+	public class BodyPart : MonoBehaviour 
+	{
+		public enum BSlot {Head, Chest, Arms, Hands, Legs, Feet};
+		public BSlot Slot;
+	}
+}
---
+++ Assets/Scripts/EquipmentSystem/FullBody.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EasyEquipmentSystem
+{
+	public class FullBody : MonoBehaviour 
+	{
+		// empty
+	}
+}
