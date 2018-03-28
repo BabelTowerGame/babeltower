7bae99104f120598108d806debd6952760aed7d6
bowiegou <i@bowiegou.com>
2018-03-27 20:47:46 -0400

    added male and female characters

---
+++ Assets/Adam&Eva_Playble_Heroes/Adam/Adventurer/EquipmentSystem/EquipmentSystemInspectorGUI.cs/Equipment.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EasyEquipmentSystem
+{
+	public class Equipment : MonoBehaviour 
+	{	
+		public enum EquipType { Armor, Weapon };
+		public enum EquipArmorSlot {HandRight, HandLeft, BackWeaponR, BackWeaponL, BackShield, Head, Hair, Chest, Shoulders, Arms, Hands, Belt, Legs, Feet, Extra1, Extra2, Extra3};
+		public EquipType EquipmentType;
+		public EquipArmorSlot EquipmentSlot;
+	}
+}
