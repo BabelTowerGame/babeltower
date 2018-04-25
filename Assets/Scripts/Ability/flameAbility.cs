463e5be2f88f51c83ad79a51c4e0c5d124162403
Charles Ma <1995charles@gmail.com>
2018-04-25 14:47:33 -0400

    Import particle effect asset and create flame ability

---
+++ Assets/Scripts/Ability/flameAbility.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+	[CreateAssetMenu (menuName = "Ability/flameAbility")]
+	public class flameAbility : Ability {
+
+		public float damage = 2;
+		public float range = 10f;
+
+		private flameAbilityTrigger tt;
+		public override void Initialize(GameObject obj) {
+			tt = obj.GetComponent<flameAbilityTrigger> ();
+			tt.damage = damage;
+			tt.range = range;
+		}
+
+		public override void TriggerAbility(bool buttonPressed) {
+			tt.launch (buttonPressed);
+		}
+	}
+}
