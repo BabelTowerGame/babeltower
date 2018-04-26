3f065dcaed7266e6df4c6292b9e99d302bd6b758
bowiegou <i@bowiegou.com>
2018-04-26 12:30:07 -0400

    added logic for animation network sync; added character controller for non-local character

--- Assets/Scripts/Network/NetworkIDController.cs
+++ Assets/Scripts/Network/NetworkIDController.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 using UnityStandardAssets.Characters.ThirdPerson;
 
 public class NetworkIDController : MonoBehaviour {
+    //Note that for Character that are not local player
+    //A Character Controller will be assigned
+    //Animator will be handled serperately
+    private CharacterController charController;
+    private Animator charAnimator;
 
-    private ThirdPersonCharacter charController;
     private NetworkID networkID;
+
+
     private Vector3 prevPosition;
     private Quaternion prevRotation;
+
+
+    private int[] prevAnimationHash;
+    private int[] prevAnimationTransitionHash;
+
+
 
     private Vector3 targetPosition;
     private Quaternion targetRotation;
@@ -22,7 +34,7 @@ public class NetworkIDController : MonoBehaviour {
     const float k_LocalMovementThreshold = 0.00001f;
     const float k_LocalRotationThreshold = 0.00001f;
     const float k_NetWorkTickRate = 0.1f;
-    const float k_snapThreshold = 0.1f;
+    const float k_snapThreshold = 10.0f;
     const float k_interpolateMovement = 0.1f;
     const float k_interpolateRotation = 0.1f;
 
@@ -32,24 +44,30 @@ public class NetworkIDController : MonoBehaviour {
 
     // Use this for initialization
     void Awake() {
-	    charController = GetComponent<ThirdPersonCharacter>();
+	    charController = GetComponent<CharacterController>();
+        charAnimator = GetComponent<Animator>();
         networkID = GetComponent<NetworkID>();
 	    prevPosition = transform.position;
 	    prevRotation = transform.rotation;
+        prevAnimationHash = new int[charAnimator.layerCount];
+        prevAnimationTransitionHash = new int[charAnimator.layerCount];
 
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update() {
+        //networkTickrate enforcement
+	    if (Time.time - lastClientSendTime < k_NetWorkTickRate) return;
 	    if (networkID.IsLocalPlayer) {
 	        SendTransform();
 
-            //TODO: May need a way to enforce server tickrate OR Do we really need a tickrate for client?
 	        lastClientSendTime = Time.time;
 	    }
 	}
 
     void FixedUpdate() {
+
         //FixedUpdate for local player are controller by character controller
         if (!networkID.IsLocalPlayer) {
             UpdateTransform();
@@ -84,16 +102,58 @@ public class NetworkIDController : MonoBehaviour {
 
         prevPosition = transform.position;
         prevRotation = transform.rotation;
+    }
+
+    void SendAnimation() {
+        int[] stateHash = new int[charAnimator.layerCount];
+        float[] normalizedTime = new float[charAnimator.layerCount];
+        bool hasChanged = false;
+        for (int i = 0; i < charAnimator.layerCount; i++) {
+            if (CheckAnimStateChanged(i, out stateHash[i], out normalizedTime[i])) {
+                hasChanged = true;
+            }
+        }
+        if(!hasChanged) return;
+        //TODO: Build network message using Hash and Time array above
+        //TODO: Send msg to network
+    }
 
 
+    bool CheckAnimStateChanged(int layer,out int stateHash, out float normalizedTime) {
+        stateHash = 0;
+        normalizedTime = 0;
 
+        if (charAnimator.IsInTransition(layer)) {
+            AnimatorTransitionInfo tt = charAnimator.GetAnimatorTransitionInfo(layer);
+            if (tt.fullPathHash != prevAnimationTransitionHash[layer]) {
+                // first time in this transition
+                prevAnimationTransitionHash[layer] = tt.fullPathHash;
+                prevAnimationHash[layer] = 0;
+                return true;
+            }
+            return false;
+        }
+
+        AnimatorStateInfo st = charAnimator.GetCurrentAnimatorStateInfo(layer);
+        if (st.fullPathHash != prevAnimationHash[layer]) {
+            // first time in this animation state
+            if (prevAnimationHash[layer] != 0) {
+                // came from another animation directly - from Play()
+                stateHash = st.fullPathHash;
+                normalizedTime = st.normalizedTime;
+            }
+            prevAnimationTransitionHash[layer] = 0;
+            prevAnimationHash[layer] = st.fullPathHash;
+            return true;
+        }
+        return false;
     }
 
     void UpdateTransform() {
 
         if (fixedPosDiff == Vector3.zero && targetRotation == transform.rotation)
             return;
-        charController.Move(fixedPosDiff * k_interpolateMovement, targetCrouch, targetJump, false);
+        charController.Move(fixedPosDiff * k_interpolateMovement);
 
         transform.rotation = Quaternion.Slerp(
             transform.rotation,
@@ -107,15 +167,13 @@ public class NetworkIDController : MonoBehaviour {
             fixedPosDiff = Vector3.zero;
 
             var diff = targetPosition - transform.position;
-            charController.Move(diff,false,false,false);
+            charController.Move(diff);
         }
-
-
-
 
     }
 
-    public void onReceiveMessage() {
+    //TODO:parameter message
+    public void onReceiveMovement() {
         //Save Network Message in a Buffer for next Fixed update
         if (networkID.IsLocalPlayer) {
             //LocalPlayer does not need to be updated by network
@@ -134,5 +192,39 @@ public class NetworkIDController : MonoBehaviour {
             transform.position = targetPosition;
         }
         lastClientSyncTime = Time.time;
+    }
+
+    //TODO:parameter message
+    public void onReceiveAnimation() {
+        //Animator will be updated in realtime once the message 
+        //is receive since no sync issue is involved
+
+        if (networkID.IsLocalPlayer)
+            return;
+
+        //TODO parse messageto stateHash and normalizedTime
+        //Since we have only two layers this will be good enough
+        //charAnimator.Play(msg.layer[0].stateHash, 0, msg.layer[0].normalizedTime);
+        //charAnimator.Play(msg.layer[1].stateHash, 1, msg.layer[1].normalizedTime);
+
+        for (int i = 0; i < charAnimator.parameters.Length; i++) {
+
+            AnimatorControllerParameter par = charAnimator.parameters[i];
+            if (par.type == AnimatorControllerParameterType.Int) {
+                int newValue = 0; //TODO: read from message
+                charAnimator.SetInteger(par.nameHash, newValue);
+            }
+
+            if (par.type == AnimatorControllerParameterType.Float) {
+                float newFloatValue = 0; //TODO: read from message
+                charAnimator.SetFloat(par.nameHash, newFloatValue);
+            }
+
+            if (par.type == AnimatorControllerParameterType.Bool) {
+                bool newBoolValue = false; //TODO: read from message
+                charAnimator.SetBool(par.nameHash, newBoolValue);
+            }
+        }
+
     }
 }
