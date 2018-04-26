f78195a993a859da097946c01aef23dca89d0bee
Charles Ma <1995charles@gmail.com>
2018-04-26 00:47:49 -0400

    Create player when game start and apply appearance setting

---
+++ Assets/Scripts/CreateCharacter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCharacter : MonoBehaviour {
+
+	void Awake(){
+		Character c = CharacterManager.character;
+
+		Debug.Log (transform.position);
+
+		Vector3 point = new Vector3 (Random.Range (1000, 1300), 1000, Random.Range (1000, 1300));
+
+		RaycastHit hit;
+		// note that the ray starts at 100 units
+
+		if (Physics.Raycast(point, -Vector3.up, out hit)) { 
+			Debug.Log ("hit");
+			Debug.Log (hit.point);
+		}
+
+		Vector3 spawn = hit.point;
+
+		spawn.y += 10;
+
+		if (c.appearance.gender == Appearance.Gender.Male) {
+			GameObject go = GameObject.Instantiate (Resources.Load ("Prefabs/PlayerModels/MaleCharacterController"), spawn, Quaternion.identity) as GameObject;
+			go.GetComponent<EasyEquipmentSystem.EquipmentSystem> ().onHairColorChanged (c.appearance.hairColor);
+		}
+
+	}
+}
